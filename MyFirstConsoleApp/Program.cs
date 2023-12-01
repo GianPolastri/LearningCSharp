5d665cd0d452ce71e3b5b673bf703681ee7f4faf
GianPolastri <gianpolastri@gmial.com>
2023-12-01 13:31:35 -0300

    Add project files.

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿
+public class MyFirstConsoleApp
+{
+  
+    static void Main(string[] args)
+    {
+        WriteSomething();
+        WriteSomethingSpecific("I am an argument and called form a method");
+        Console.Read();
+    }
+
+    //Access modifier (static) return type method name (parameter1, parameter2)
+    public static void WriteSomething()
+    {
+        Console.WriteLine("I am  called form a Method");
+    }
+
+    public static void WriteSomethingSpecific(string myText)
+    {
+        Console.WriteLine(myText);
+    }
+}
+ 
