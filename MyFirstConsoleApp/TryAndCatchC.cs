0613433129c86354a04a83598856c0b8a197df8b
GianPolastri <gianpolastri@gmial.com>
2023-12-04 18:19:29 -0300

    Made a method that uses user inputs and try & catch.

--- MyFirstConsoleApp/TryAndCatchC.cs
+++ MyFirstConsoleApp/TryAndCatchC.cs
@@ -10,6 +10,7 @@ namespace TryAndCatchC
     {
         static void Main(string[] args)
         {
+            /*
             Console.WriteLine("Please enter a number!");
             string userInput = Console.ReadLine();
 
@@ -37,6 +38,36 @@ namespace TryAndCatchC
 
 
             Console.ReadKey();
+            */
+
+            try
+            {
+                Console.WriteLine("Welcome to the divide calculator! \n Please enter the first number: ");
+                string num1Input = Console.ReadLine();
+                Console.WriteLine("Enter the second number: ");
+                string num2Input = Console.ReadLine();
+                int num1 = int.Parse(num1Input);
+                int num2 = int.Parse(num2Input);
+                double result = num1 / num2;
+                Console.WriteLine("The result is: " + result);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Format exception, please enter the correct type next time."); ;
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Overflow exception, the number was too long or too short for an int32");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("ArgumentNullException, the value was empty (null)");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("DivideByZeroException, It is not allowed to divide by zero.");
+            }
+
         }
     }
 }
